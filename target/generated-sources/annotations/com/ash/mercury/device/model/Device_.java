b62137aff0625bf17598f6e044797131a01a4c73
DeSsMon7 <dessmon7@gmail.com>
2018-04-16 20:36:58 +0300

    update OfficeOrders
    -update editForms

--- target/generated-sources/annotations/com/ash/mercury/device/model/Device_.java
+++ target/generated-sources/annotations/com/ash/mercury/device/model/Device_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-12T17:40:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-16T20:21:56")
 @StaticMetamodel(Device.class)
 public class Device_ { 
 
--- target/generated-sources/annotations/com/ash/mercury/servicepart/model/ServicePart_.java
+++ target/generated-sources/annotations/com/ash/mercury/servicepart/model/ServicePart_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-12T17:40:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-16T20:21:56")
 @StaticMetamodel(ServicePart.class)
 public class ServicePart_ { 
 
