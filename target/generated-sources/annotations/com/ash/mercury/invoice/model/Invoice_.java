82e16a9699bedb1a0b82af7196b6d4019ce433c9
DeSsMon7 <dessmon7@gmail.com>
2018-04-20 20:59:13 +0300

    update packages
    -minor update on packages

--- target/generated-sources/annotations/com/ash/mercury/invoice/model/Invoice_.java
+++ target/generated-sources/annotations/com/ash/mercury/invoice/model/Invoice_.java
@@ -9,7 +9,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-16T20:21:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-20T20:46:55")
 @StaticMetamodel(Invoice.class)
 public class Invoice_ { 
 
