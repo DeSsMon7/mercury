2ca394f6f9b17bd69027377fdf3640d9c4a31f8f
DeSsMon7 <dessmon7@gmail.com>
2018-05-02 20:10:56 +0300

    update OfficeOrder
    -add Office Order Details

--- target/generated-sources/annotations/com/ash/mercury/customer/model/Customer_.java
+++ target/generated-sources/annotations/com/ash/mercury/customer/model/Customer_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-20T20:46:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-02T19:46:29")
 @StaticMetamodel(Customer.class)
 public class Customer_ { 
 
--- target/generated-sources/annotations/com/ash/mercury/officeorder/model/OfficeOrder_.java
+++ target/generated-sources/annotations/com/ash/mercury/officeorder/model/OfficeOrder_.java
@@ -11,7 +11,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-20T20:46:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-02T19:46:29")
 @StaticMetamodel(OfficeOrder.class)
 public class OfficeOrder_ { 
 
