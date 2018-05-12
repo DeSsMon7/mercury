769771c5aa5b6be464a1ba5fe45f42870b1b7bea
DeSsMon7 <dessmon7@gmail.com>
2018-05-12 13:46:34 +0300

    update Office Details
    -fix no display data bugs in Details Form

--- target/generated-sources/annotations/com/ash/mercury/invoice/model/Invoice_.java
+++ target/generated-sources/annotations/com/ash/mercury/invoice/model/Invoice_.java
@@ -9,7 +9,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-11T18:56:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-12T13:34:25")
 @StaticMetamodel(Invoice.class)
 public class Invoice_ { 
 
